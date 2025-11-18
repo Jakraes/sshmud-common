a8399735d8b0fcaae0d1656b8c1e527204d56a4b
Jakraes <jpcarvalhoyolo@gmail.com>
2025-11-18 02:29:18 +0000

    Started map-entity relationship

---
+++ include/sshmud/map_entity.h
@@ -0,0 +1,9 @@
+#ifndef MAP_ENTITY_H
+#define MAP_ENTITY_H
+
+#include "map.h"
+#include "entity.h"
+
+int entity_move(entity_t *self, map_t *map);
+
+#endif
---
+++ src/map_entity.c
@@ -0,0 +1,12 @@
+#include "sshmud/map_entity.h"
+
+#include "sshmud/util.h"
+
+int entity_move(entity_t *self, map_t *map) {
+    // TODO
+
+    UNUSED(self);
+    UNUSED(map);
+
+    return 0;
+}
